63dae2391e3922448a706cf435f013902f611350
kamal heydari <kamal3heydari@gmail.com>
2022-05-29 11:27:58 +0430

    chapter 7

--- pages/cart.js
+++ pages/cart.js
@@ -4,10 +4,53 @@ import { useContext, useState, useEffect } from "react";
 import { DataContext } from "../store/GlobalState";
 import { getData, postData } from "../utils/fetchData";
 import { useRouter } from "next/router";
+import CartItem from "../components/CartItem";
 
 export default function Cart() {
   const { state, dispatch } = useContext(DataContext);
-  const { cart } = state;
+  const { cart, auth } = state;
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const getTotal = () => {
+      const res = cart.reduce((prev, item) => {
+        return prev + item.price * item.quantity;
+      }, 0);
+
+      setTotal(res);
+    };
+
+    getTotal();
+  }, [cart]);
+
+  useEffect(() => {
+    const cartLocal = JSON.parse(localStorage.getItem("__next__cart01__devat"));
+    if (cartLocal && cartLocal.length > 0) {
+      let newArr = [];
+      const updateCart = async () => {
+        for (const item of cartLocal) {
+          const res = await getData(`product/${item._id}`);
+          const { _id, title, images, price, inStock, sold } = res.product;
+          if (inStock > 0) {
+            newArr.push({
+              _id,
+              title,
+              images,
+              price,
+              inStock,
+              sold,
+              quantity: item.quantity > inStock ? 1 : item.quantity,
+            });
+          }
+        }
+
+        dispatch({ type: "ADD_CART", payload: newArr });
+      };
+
+      updateCart();
+    }
+  }, []);
 
   if (cart.length === 0)
     return (
@@ -18,5 +61,66 @@ export default function Cart() {
       />
     );
 
-  return <div>Cart</div>;
+  return (
+    <div className='row mx-auto'>
+      <Head>
+        <title>Cart Page</title>
+      </Head>
+
+      <div className='col-md-8 text-secondary table-responsive my-3'>
+        <h2 className='text-uppercase'>Shopping Cart</h2>
+
+        <table className='table my-3'>
+          <tbody>
+            {cart.map((item) => (
+              <CartItem
+                key={item._id}
+                item={item}
+                dispatch={dispatch}
+                cart={cart}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className='col-md-4 my-3 text-right text-uppercase text-secondary'>
+        <form>
+          <h2>Shipping</h2>
+
+          <label htmlFor='address'>Address</label>
+          <input
+            type='text'
+            name='address'
+            id='address'
+            className='form-control mb-2'
+            // value={address}
+            // onChange={(e) => setAddress(e.target.value)}
+          />
+
+          <label htmlFor='mobile'>Mobile</label>
+          <input
+            type='text'
+            name='mobile'
+            id='mobile'
+            className='form-control mb-2'
+            // value={mobile}
+            // onChange={(e) => setMobile(e.target.value)}
+          />
+        </form>
+
+        <h3>
+          Total: <span className='text-danger'>${total}</span>
+        </h3>
+
+        <Link href={auth.user ? "#!" : "/signin"}>
+          <a
+            className='btn btn-dark my-2'
+            // onClick={handlePayment}
+          >
+            Proceed with payment
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 }
